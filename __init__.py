a27d1a6595aa5e2462b52729c1239db0b7e98f04
Vivek Thazhathattil <vivek.thazhathattil@gmail.com>
2021-05-30 19:08:19 +0530

    add configure option for template file

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import shutil
+import uuid
 
 from gi.repository import GObject, Gtk, Gdk
 
@@ -28,6 +29,7 @@ class XournalppPlugin(PluginClass):
     plugin_preferences = (
         # key, type, label, default
         ("xopp_dir", "string", _("Xopp Directory"), "~"),
+        ("template_file", "string", _("Xournalpp template file path"), ""),
         ("clean_xopp_dir", "bool", _("Clean Xopp directory after use"), False),
     )
 
@@ -45,7 +47,9 @@ class XournalppMainWindowExtension(MainWindowExtension):
     def __init__(self, plugin, window):
         super().__init__(plugin, window)
         self.xopp_dir = self.plugin.preferences["xopp_dir"]
+        self.template_file = self.plugin.preferences["template_file"]
         self.clean_dir_pref = self.plugin.preferences["clean_xopp_dir"]
+        self.template_valid = False # by default use the manual saving option
         self.xournalpp = None
         self.imagemagick = None
 
@@ -53,13 +57,20 @@ class XournalppMainWindowExtension(MainWindowExtension):
         full_path = os.path.expanduser(self.xopp_dir)
         return os.path.isdir(full_path)
 
+    def is_template_file_valid(self):
+        full_path = ""
+        if self.template_file is not None and self.template_file.endswith(".xopp"):
+            full_path = os.path.expanduser(self.template_file)
+        return os.path.isfile(full_path)
+
     @action(_("_Xournalpp drawing"), accelerator="<ctrl>m", menuhints="insert:Drawing")
     def Xournalpp(self):
         if not self.is_xopp_dir_valid():
-            raise AssertionError("Invalid Xopp Directory")
+            raise AssertionError("Invalid Xopp directory")
             return
 
-        self.setup_dialog()
+        self.template_valid = self.is_template_file_valid()
+        #self.setup_dialog()
         self.run_xournalpp()
         last_mod_file_path = self.get_last_modified_file()
         if last_mod_file_path is None:
@@ -81,11 +92,22 @@ class XournalppMainWindowExtension(MainWindowExtension):
 
     def run_xournalpp(self):
         try:
-            self.xournalpp = Application(xournalppcmd)
+            if (self.template_valid):
+                # open template
+                new_file_path = copy_template_to_xopp_dir()
+                self.xournalpp = Application(xournalppcmd + " " + new_file_path)
+            else:
+                self.xournalpp = Application(xournalppcmd)
             self.xournalpp.run()
         except ApplicationError:
             # log should have details of failure
             return None, logfile
+
+    def copy_template_to_xopp_dir(self):
+        curr_file_path = self.template_file
+        dest_file_path = os.path.join(os.path.expanduser(self.xopp_dir), str(uuid.uuid4()) + ".xopp") # generate random file_name
+        shutil.copy(curr_file_path, dest_file_path)
+        return dest_file_path
 
     def get_last_modified_file(self):
         full_path = os.path.expanduser(self.xopp_dir)
@@ -98,10 +120,9 @@ class XournalppMainWindowExtension(MainWindowExtension):
 
     def prepare_img(self, xopp_file):
         # get png file out of xopp
-        #raw_img_file_name = os.path.basename(xopp_file)[:-5] + ".png"
-        #self.tmp_file = TmpFile(raw_img_file_name)
+        # raw_img_file_name = os.path.basename(xopp_file)[:-5] + ".png"
+        # self.tmp_file = TmpFile(raw_img_file_name)
         raw_img_file = xopp_file[:-5] + ".png"
-        print(raw_img_file)
         xopp2png_cmd = ("xournalpp", xopp_file, "-i", raw_img_file)
         self.xournalpp = Application(xopp2png_cmd)
         self.xournalpp.run()
@@ -132,23 +153,23 @@ class XournalppMainWindowExtension(MainWindowExtension):
         self.imagemagick = Application(cropped_transparent_img_cmd)
         self.imagemagick.run()
 
-        return(cropped_transparent_img_file)
+        return cropped_transparent_img_file
 
     def show_img_editor(self):
         print("placeholder")
-    
+
     def clean_xopp_dir(self):
         full_path = os.path.expanduser(self.xopp_dir)
         test = os.listdir(full_path)
         for file in test:
             if file.endswith(".xopp") or file.endswith(".png"):
                 os.remove(os.path.join(full_path, file))
+
     def insert_image(self, curr_file_path):
         page = self.window.pageview.page
         notebook = self.window.notebook
         dest_dir = notebook.get_attachments_dir(page)
         dest_file_name = os.path.basename(curr_file_path)
-        dest_file_path = os.path.join(dest_dir.path,dest_file_name)
-        print(dest_file_path)
+        dest_file_path = os.path.join(dest_dir.path, dest_file_name)
         shutil.copy(curr_file_path, dest_file_path)
         self.window.pageview.insert_image(File(dest_file_path))
